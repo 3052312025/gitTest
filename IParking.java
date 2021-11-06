0caa6d77f554c51ec81e6dd60137e9bcb20954a1
黄松凯 <2714536440@qq.com>
2021-11-06 15:10:52 +0800

    Test2

---
+++ IParking.java
@@ -0,0 +1,9 @@
+package gitTest;
+
+import java.util.ArrayList;
+
+public interface IParking {
+    public void print( String a[]);
+    public boolean addCar(int carType);
+    public String[] parse(String s);
+}
